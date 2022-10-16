be486ec0fed31d2813b00bd82aace9fd3af7fdc6
Alexandr <drk91@inbox.ru>
2022-10-16 13:17:28 +0300

    Create project

---
+++ notion/notion/notionApp.swift
@@ -0,0 +1,20 @@
+//
+//  notionApp.swift
+//  notion
+//
+//  Created by Александр Ломтев on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct notionApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
